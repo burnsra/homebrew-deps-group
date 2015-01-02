3482a19a56a4eb2fd0b08e60433db46f715b4498
Shulhi Sapli <shulhi@gmail.com>
2015-01-02 19:31:26 +0800

    Update formula

--- Formula/brew-deps-group.rb
+++ Formula/brew-deps-group.rb
@@ -21,11 +21,11 @@ class BrewDepsGroup < Formula
       end
     end
 
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+    # ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
     # system "python", *Language::Python.setup_install_args(libexec)
 
-    bin.install Dir[libexec/"bin/*"]
-    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+    # bin.install Dir[libexec/"bin/*"]
+    # bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
 
     bin.install 'brew-deps-group.py'
     bin.install 'brew-deps-group.rb'
